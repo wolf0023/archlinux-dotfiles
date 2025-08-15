4bae5839ba9f2e747fa91c686a66e4e31f04ac76
wolf0023 <wolf0023@ymt.bbiq.jp>
2025-08-15 14:56:03 +0900

    nvimのエラーや診断メッセージを表示するように変更

--- nvim/lua/plugins/mason.nvim.lua
+++ nvim/lua/plugins/mason.nvim.lua
@@ -9,5 +9,33 @@ return {
     },
     {
         "neovim/nvim-lspconfig",
+        config = function()
+        -- 診断メッセージの表示設定
+            vim.diagnostic.config({
+                virtual_text = true,
+                signs = true,
+                underline = true,
+                update_in_insert = false,
+                severity_sort = true,
+            })
+            -- ホバー時に診断メッセージを表示
+            vim.api.nvim_create_autocmd("CursorHold", {
+                callback = function()
+                    vim.diagnostic.open_float({
+                        focusable = false,
+                        source = 'always',
+                        scope = 'cursor',
+                        header = '',
+                        prefix = ' ',
+                        close_events = {
+                            'CursorMoved',
+                            'InsertEnter',
+                            'FocusLost',
+                            'BufLeave',
+                        }
+                    })
+                end
+            })
+        end
     }
 }
