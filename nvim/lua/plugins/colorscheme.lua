e47ccf70abed4ccdaff12d7141fb5a74125689d3
wolf0023 <wolf0023@ymt.bbiq.jp>
2025-09-10 01:45:53 +0900

    ターミナルとnvimの透明効果を無効に変更

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,10 +3,10 @@ return {
         "catppuccin/nvim",
         config = function()
             require("catppuccin").setup({
-                transparent_background = true,
                 flavour = "mocha",
                 inetegrations = {
                     auto_integrations = true,
+                    mason = true,
                 }
             })
         end
