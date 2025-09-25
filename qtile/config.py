774c03b18b90d5e44f104e32edbc9ad923cfaf24
wolf0023 <wolf0023@ymt.bbiq.jp>
2025-09-25 16:38:43 +0900

    アクティブウィンドウをカーソルを合わせたウィンドウに変更するように設定

--- qtile/config.py
+++ qtile/config.py
@@ -116,7 +116,7 @@ mouse = [
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: list
-follow_mouse_focus = False
+follow_mouse_focus = True
 bring_front_click = False
 floats_kept_above = True
 cursor_warp = False
