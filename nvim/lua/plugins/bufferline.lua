23d73e82b661cdeae46fdc335459d80cf5eeb7c5
wolf0023 <wolf0023@ymt.bbiq.jp>
2025-09-29 18:31:22 +0900

    依存関係を修正

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -3,7 +3,7 @@ return {
 		"akinsho/bufferline.nvim",
 		config = function()
 			require("bufferline").setup({
-                highlights = require("catppuccin.groups.integrations.bufferline").get_theme()
+                highlights = require("catppuccin.special.bufferline").get_theme()
             })
 		end
 	},
