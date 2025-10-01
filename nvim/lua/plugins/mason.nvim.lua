23523978fa9e89f1db4777276db338262869b87e
wolf0023 <wolf0023@ymt.bbiq.jp>
2025-10-01 13:11:15 +0900

    ウィンドウ枠のスタイルを変更。警告部分にカーソルを当てたときのみ診断メッセージを表示するように修正。

--- nvim/lua/plugins/mason.nvim.lua
+++ nvim/lua/plugins/mason.nvim.lua
@@ -10,35 +10,33 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
-        -- 診断メッセージの表示設定
-        vim.diagnostic.config({
-            virtual_text = true,
-            signs = true,
-            underline = true,
-            update_in_insert = false,
-            severity_sort = true,
-            float = {
-                border = 'rounded',
-                source = 'always',
-                header = '',
-                prefix = ' ',
-            }
-        })
-
-        -- ホバー時に診断メッセージを表示
-        vim.api.nvim_create_autocmd("CursorHold", {
-            callback = function()
-                vim.diagnostic.open_float({
-                    focusable = false,
-                    close_events = {
-                        'CursorMoved',
-                        'InsertEnter',
-                        'FocusLost',
-                        'BufLeave',
-                    }
-                })
-            end
+            -- 診断メッセージの表示設定
+            vim.diagnostic.config({
+                virtual_text = true,
+                signs = true,
+                underline = true,
+                update_in_insert = false,
+                severity_sort = true,
             })
+
+            -- LSPのhoverや診断メッセージのウィンドウの枠をroundedにする
+            vim.o.winborder = 'rounded'
+
+            -- ホバー時に診断メッセージを表示
+            vim.api.nvim_create_autocmd("CursorHold", {
+                callback = function()
+                    vim.diagnostic.open_float({
+                        focusable = false,
+                        scope= 'cursor',
+                        close_events = {
+                            'CursorMoved',
+                            'InsertEnter',
+                            'FocusLost',
+                            'BufLeave',
+                        }
+                    })
+                end
+                })
         end
     }
 }
