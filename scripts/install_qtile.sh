04ed3c97d7955f98964e554e272b733542a8569b
wolf0023 <wolf0023@ymt.bbiq.jp>
2025-11-04 01:16:52 +0900

    uv環境が初期化済みの場合にスキップするように変更

--- scripts/install_qtile.sh
+++ scripts/install_qtile.sh
@@ -9,17 +9,21 @@ cd "$HOME" || {
     log "Could not change directory to home: $HOME" "error" "==> "
 }
 
-# uvプロジェクトの初期化
-uv init -p pypy3 uv-qtile || {
-    log "Failed to initialize uvtool for qtile." "error" "==> "
-}
+# uv-qtileが既に存在する場合はスキップ
+if [ ! -d "uv-qtile" ]; then
+    log "Initializing uvtool project for qtile..." "info" ":: "
+    # uvプロジェクトの初期化
+    uv init -p pypy3 uv-qtile || {
+        log "Failed to initialize uvtool for qtile." "error" "==> "
+    }
+fi
 
 # venv環境を構築し、アクティベート
 cd uv-qtile || {
     log "Could not change directory to uv-qtile: $HOME/uv-qtile" "error" "==> "
 }
 uv sync || {
-    log "Failed to sync uvtool packages for qtile." "error" "==> "
+    log "Failed to create uvtool virtual environment for qtile." "error" "==> "
 }
 source .venv/bin/activate || {
     log "Failed to activate uvtool virtual environment for qtile." "error" "==> "
