5dc71f2b6fdb4458e473f36967d70629c4f752ed
wolf0023 <wolf0023@ymt.bbiq.jp>
2025-11-03 20:17:23 +0900

    バックアップ時に圧縮してから移動するように変更

--- scripts/functions/set_config_link.sh
+++ scripts/functions/set_config_link.sh
@@ -3,23 +3,37 @@
 # $2: リンク先ファイル
 # $3: バックアップディレクトリ
 function setConfigLink {
-    local source_file="$1"
-    local target_file="$2"
+    local source_path="$1"
+    local target_path="$2"
     local backup_dir="$3"
 
     # ソースファイルの存在チェック
-    checkFileExists "$source_file" || {
-        log "Source file does not exist: $source_file" "error" "  --> "
+    checkFileExists "$source_path" || {
+        log "Source file does not exist: $source_path" "error" "  --> "
         return 1
     }
 
     # 既存のファイルやディレクトリをバックアップ
-    if [ -e "$target_file" ]; then
-        mv "$target_file" "${backup_dir}${target_file}"
+    if [ -e "$target_path" ]; then
+        local filename
+        filename="${target_path##*/}-$(date +%Y%m%d%H%M%S).tar.gz"
+
+        tar czf "$filename" "$target_path" || {
+            log "Failed to backup existing file or directory: $target_path" "error" "  --> "
+            return 1
+        }
+        mv "$filename" "$backup_dir/" || {
+            log "Failed to move backup to backup directory: $backup_dir" "error" "  --> "
+            return 1
+        }
+        rm -rf "$target_path" || {
+            log "Failed to remove existing file or directory after backup: $target_path" "error" "  --> "
+            return 1
+        }
     fi
 
-    ln -sf --backup=existing "$source_file" "$target_file" || {
-        log "Failed to create symbolic link from $source_file to $target_file" "error" "  --> "
+    ln -sf "$source_path" "$target_path" || {
+        log "Failed to create symbolic link from $source_path to $target_path" "error" "  --> "
         return 1
     }
 }
