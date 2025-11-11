9a02407b2c996182ebf89857fe6c2094d63bafe8
wolf0023 <wolf0023@ymt.bbiq.jp>
2025-11-11 21:09:45 +0900

    fcitx5との連携をスムーズにするための、スクリプトを追加

--- configs/common/nvim/init.lua
+++ configs/common/nvim/init.lua
@@ -4,4 +4,5 @@ require("config.lazy")
 -- 設定ファイル
 require("config.keybind")
 require("config.option")
+require("config.fcitx5")
 
---
+++ configs/common/nvim/lua/config/fcitx5.lua
@@ -0,0 +1,36 @@
+local function trim(s)
+    return s:match("^%s*(.-)%s*$")
+end
+
+local function GetFcitxStatus()
+    local handle = io.popen('fcitx5-remote -n')
+    local result = handle:read("*a")
+    handle:close()
+    return trim(result)
+end
+
+local function OnInsertLeave()
+    -- Save fcitx status when leaving insert mode
+    vim.g.fcitx_status = GetFcitxStatus()
+    -- Disable (Japanese) input method 
+    os.execute('fcitx5-remote -c')
+end
+
+local function Enable(status)
+    if status ~= 'keyboard-us' then
+        os.execute('fcitx5-remote -o')
+    end
+end
+
+vim.g.fcitx_status = GetFcitxStatus()
+
+-- Autocmd for InsertLeave and InsertEnter
+vim.api.nvim_create_autocmd("InsertLeave", {
+    callback = OnInsertLeave
+})
+vim.api.nvim_create_autocmd("InsertEnter", {
+    callback = function()
+        Enable(vim.g.fcitx_status)
+    end
+})
+
