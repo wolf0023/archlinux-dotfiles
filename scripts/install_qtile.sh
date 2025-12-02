1d1d4bef0c3d45943087c69f0171835988d7595c
wolf0023 <wolf0023@ymt.bbiq.jp>
2025-12-03 01:23:44 +0900

    エラー発生時に、処理が継続されてしまう問題を修正

--- scripts/install_qtile.sh
+++ scripts/install_qtile.sh
@@ -7,6 +7,7 @@ log "Installing qtile using uvtool..." "info" ":: "
 current_dir=$(pwd)
 cd "$HOME" || {
     log "Could not change directory to home: $HOME" "error" "==> "
+    return 1
 }
 
 # uv-qtileが既に存在する場合はスキップ
@@ -15,26 +16,32 @@ if [ ! -d "uv-qtile" ]; then
     # uvプロジェクトの初期化
     uv init -p pypy3 uv-qtile || {
         log "Failed to initialize uvtool for qtile." "error" "==> "
+        return 1
     }
 fi
 
 # venv環境を構築し、アクティベート
 cd uv-qtile || {
-    log "Could not change directory to uv-qtile: $HOME/uv-qtile" "error" "==> "
+    log "Could not change directory to uv-qtile: $HOME/uv-qtile" "error" "  --> "
+    return 1
 }
 uv sync || {
-    log "Failed to create uvtool virtual environment for qtile." "error" "==> "
+    log "Failed to create uvtool virtual environment for qtile." "error" "  --> "
+    return 1
 }
 source .venv/bin/activate || {
-    log "Failed to activate uvtool virtual environment for qtile." "error" "==> "
+    log "Failed to activate uvtool virtual environment for qtile." "error" "  --> "
+    return 1
 }
 
 # uvtoolを使ってqtileをインストール
 uv add qtile[widgets] || {
-    log "Failed to install qtile using uvtool." "error" "==> "
+    log "Failed to install qtile using uvtool." "error" "  --> "
+    return 1
 }
 
 # 元のディレクトリに戻る
 cd "$current_dir" || {
-    log "Could not change directory to the original directory: $current_dir" "error" "==> "
+    log "Could not change directory to the original directory: $current_dir" "error" "  --> "
+    return 1
 }
