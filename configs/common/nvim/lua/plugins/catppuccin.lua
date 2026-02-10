4577f3dea21e47122bd9893a0273f03a3ab2705f
wolf0023 <wolf0023@ymt.bbiq.jp>
2026-02-10 18:16:47 +0900

    fix: integrations 関連の設定ミスを修正

--- configs/common/nvim/lua/plugins/catppuccin.lua
+++ configs/common/nvim/lua/plugins/catppuccin.lua
@@ -4,11 +4,8 @@ return {
         config = function()
             require("catppuccin").setup({
                 flavour = "mocha",
-                inetegrations = {
-                    auto_integrations = true,
-                    mason = true,
-                },
-                transparent_background = true
+                auto_integrations = true,
+                transparent_background = true,
             })
         end
     }
