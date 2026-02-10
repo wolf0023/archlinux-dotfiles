4a824ea591d768254d1236a7a66deef0c3ec55f8
wolf0023 <wolf0023@ymt.bbiq.jp>
2026-02-10 21:17:41 +0900

    fix: plugin 読み込み後に、winborder が設定されるため、plugin がその設定を読み取れない問題を修正

--- configs/common/nvim/init.lua
+++ configs/common/nvim/init.lua
@@ -1,8 +1,11 @@
+-- 設定ファイル
+require("config.option")
+-- キーバインド
+require("config.keybind")
+
 -- lazy.nvim
 require("config.lazy")
 
--- 設定ファイル
-require("config.keybind")
-require("config.option")
+-- fcitx5 用設定
 require("config.fcitx5")
 
