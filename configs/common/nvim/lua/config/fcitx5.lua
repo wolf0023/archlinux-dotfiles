954f1b9a94e05482aa7471be008a80a2ae6af585
wolf0023 <wolf0023@ymt.bbiq.jp>
2026-02-19 20:00:54 +0900

    feat: 自作のものに変更

--- configs/common/nvim/lua/config/fcitx5.lua
+++ configs/common/nvim/lua/config/fcitx5.lua
@@ -1,38 +1,10 @@
--- Fcitx5 input method handling for Neovim
--- Citation: https://qiita.com/keith_campbell/items/6787dca658a3c9460bca
-local function trim(s)
-    return s:match("^%s*(.-)%s*$")
-end
-
-local function GetFcitxStatus()
-    local handle = io.popen('fcitx5-remote -n')
-    local result = handle:read("*a")
-    handle:close()
-    return trim(result)
-end
-
-local function OnInsertLeave()
-    -- Save fcitx status when leaving insert mode
-    vim.g.fcitx_status = GetFcitxStatus()
-    -- Disable (Japanese) input method 
-    os.execute('fcitx5-remote -c')
-end
-
-local function Enable(status)
-    if status ~= 'keyboard-us' then
-        os.execute('fcitx5-remote -o')
-    end
-end
-
-vim.g.fcitx_status = GetFcitxStatus()
-
--- Autocmd for InsertLeave and InsertEnter
-vim.api.nvim_create_autocmd("InsertLeave", {
-    callback = OnInsertLeave
-})
-vim.api.nvim_create_autocmd("InsertEnter", {
+-- Vim のモードが挿入モード以外のときに fcitx5 を無効化する
+vim.api.nvim_create_autocmd({ "VimEnter", "FocusGained", "InsertLeave", "ModeChanged" }, {
+    pattern = "*",
     callback = function()
-        Enable(vim.g.fcitx_status)
-    end
+        local mode = vim.fn.mode()
+        if mode ~= "i" then
+            vim.fn.system("fcitx5-remote -c")
+        end
+    end,
 })
-
