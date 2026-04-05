af819ce71f69c1485fb214ab30c947083fd3ff67
wolf0023 <wolf0023@ymt.bbiq.jp>
2026-04-05 15:31:36 +0900

    feat: add telescope.nvim and the its related packages

---
+++ configs/nvim/lua/plugins/telescope.nvim.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        "nvim-telescope/telescope.nvim", version = "*",
+        opt = {}
+    },
+    {
+        "nvim-lua/plenary.nvim",
+        lazy = true
+    },
+    {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make"
+    },
+    {
+        "nvim-tree/nvim-web-devicons",
+        opt = {}
+    }
+}
