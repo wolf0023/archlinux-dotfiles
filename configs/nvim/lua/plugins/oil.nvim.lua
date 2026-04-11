58a60e8fba26891ad62f1c5b3d88ef761f771857
wolf0023 <wolf0023@ymt.bbiq.jp>
2026-04-11 17:27:29 +0900

    feat: add keymap for Oil

--- configs/nvim/lua/plugins/oil.nvim.lua
+++ configs/nvim/lua/plugins/oil.nvim.lua
@@ -6,6 +6,9 @@ return {
                 show_hidden = true,
             },
         },
+        keys = {
+            { "<leader>o", "<cmd>Oil<cr>", desc = "Open parent directory"},
+        },
         -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
         lazy = false,
     },
