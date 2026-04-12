c41df763327103b270dbe7dbcc5fe5cead8a411f
wolf0023 <wolf0023@ymt.bbiq.jp>
2026-04-12 15:33:23 +0900

    feat(fish): add abbreviations for git, pacman, and systemctl

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,11 +1,52 @@
 if status is-interactive
-    # vi mode を有効化
+    # Key Bindings
+    ## Enable Vi key bindings
     fish_vi_key_bindings
 
-    # normalモードでの貼り付け・コピー
+    ## p: paste, y: yank (in normal mode)
     bind -M default p fish_clipboard_paste
     bind -M default y fish_clipboard_copy
 
-    # visualモードでの選択範囲のコピー
+    ## y: yank (in visual mode)
     bind -M visual y fish_clipboard_copy
+
+    # Abbreviations
+    ## Git
+    abbr -a gs 'git status'
+    abbr -a gsw 'git switch'
+    abbr -a gst 'git stash'
+    abbr -a gsp 'git stash pop'
+    abbr -a ga 'git add'
+    abbr -a gaa 'git add .'
+    abbr -a gc 'git commit'
+    abbr -a --set-cursor gcm 'git commit -m "%"'
+    abbr -a gl 'git log'
+    abbr -a grs 'git restore --staged'
+    abbr -a gp 'git push'
+    abbr -a gpl 'git pull'
+    abbr -a gd 'git diff'
+    abbr -a gds 'git diff --staged'
+    abbr -a gb 'git branch'
+
+    ## Directory and file navigation
+    abbr -a .. 'cd ..'
+    abbr -a ... 'cd ../..'
+    abbr -a .... 'cd ../../..'
+    abbr -a hd 'cd ~'
+    abbr -a cl 'clear'
+    abbr -a ff 'fzf --height 40% --reverse --preview "bat --color=always --style=numbers --line-range=:500 {}"'
+
+    ## Package management (pacman) abbr -a pi 'sudo pacman -S'
+    abbr -a pu 'sudo pacman -Syu'
+    abbr -a ps 'sudo pacman -Ss'
+    abbr -a pr 'sudo pacman -Rs'
+    abbr -a pl 'sudo pacman -Qe'
+
+    ## System management
+    abbr -a sr 'sudo systemctl restart'
+    abbr -a ss 'sudo systemctl start'
+    abbr -a sp 'sudo systemctl stop'
+    abbr -a se 'sudo systemctl enable'
+    abbr -a sd 'sudo systemctl disable'
+    abbr -a ss 'sudo systemctl status'
 end
