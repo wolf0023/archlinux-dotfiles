6891c1061271fab143434e4a2d82c29a7b874378
wolf0023 <wolf0023@ymt.bbiq.jp>
2026-04-15 16:03:51 +0900

    feat(telescope): add keymaps for find_files, live_grep, and buffers

--- configs/nvim/lua/plugins/telescope.nvim.lua
+++ configs/nvim/lua/plugins/telescope.nvim.lua
@@ -1,6 +1,11 @@
 return {
     {
         "nvim-telescope/telescope.nvim", version = "*",
+        keys = {
+            { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+            { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+            { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers"},
+        },
         opt = {}
     },
     {
