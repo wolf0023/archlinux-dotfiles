bf1210f5b721e4677285da2caa9587d83defea18
wolf0023 <wolf0023@ymt.bbiq.jp>
2026-04-16 17:25:22 +0900

    feat(shell): replace ls with eza and overhaul systemctl aliases

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -32,9 +32,21 @@ if status is-interactive
     abbr -a .. 'cd ..'
     abbr -a ... 'cd ../..'
     abbr -a .... 'cd ../../..'
-    abbr -a hd 'cd ~'
     abbr -a cl 'clear'
+    abbr -a mk 'mkdir -p'
+    abbr -a tc 'touch'
+
+    ### fzf with bat preview
     abbr -a ff 'fzf --height 40% --reverse --preview "bat --color=always --style=numbers --line-range=:500 {}"'
+
+    ### ls and tree with icons (using eza)
+    abbr -a ls 'eza --icons'
+    abbr -a ll 'eza --icons -l'
+    abbr -a la 'eza --icons -a'
+    abbr -a lla 'eza --icons -la'
+    abbr -a lt 'eza --icons --tree'
+    abbr -a lta 'eza --icons --tree -a'
+    abbr -a ltl 'eza --icons --tree --level'
 
     ## Package management (pacman) abbr -a pi 'sudo pacman -S'
     abbr -a pu 'sudo pacman -Syu'
@@ -44,9 +56,12 @@ if status is-interactive
 
     ## System management
     abbr -a sr 'sudo systemctl restart'
-    abbr -a ss 'sudo systemctl start'
+    abbr -a st 'sudo systemctl start'
     abbr -a sp 'sudo systemctl stop'
     abbr -a se 'sudo systemctl enable'
     abbr -a sd 'sudo systemctl disable'
     abbr -a ss 'sudo systemctl status'
+
+    ## Development
+    abbr -a vi 'nvim'
 end
