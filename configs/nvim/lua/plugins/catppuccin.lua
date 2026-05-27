689056131a902349fa4b825852749d93db05529f
wolf0023 <wolf0023@ymt.bbiq.jp>
2026-05-27 18:23:02 +0900

    style(nvim): disable transparency and set catppuccin load priority

--- configs/nvim/lua/plugins/catppuccin.lua
+++ configs/nvim/lua/plugins/catppuccin.lua
@@ -1,14 +1,12 @@
 return {
     {
         "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
         config = function()
             require("catppuccin").setup({
                 flavour = "mocha",
                 auto_integrations = true,
-                transparent_background = true,
-                float = {
-                    transparent = true,
-                },
             })
             -- テーマの設定
             vim.cmd.colorscheme "catppuccin"
