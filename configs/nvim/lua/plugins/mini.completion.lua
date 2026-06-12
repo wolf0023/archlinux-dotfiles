99fa77352b09afcea3aa067fde3602ce53c4e49c
wolf0023 <wolf0023@ymt.bbiq.jp>
2026-06-12 22:17:05 +0900

    feat(nvim): replace nvim-cmp with mini.completion

---
+++ configs/nvim/lua/plugins/mini.completion.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "nvim-mini/mini.completion",
+        version = false,
+        opts = {}
+    },
+    {
+        "nvim-mini/mini.icons",
+        version = false,
+        opts = {}
+    }
+}
